2eb647f247b8a7f1bcda68e5130d2a0438374117
mdpalumbo <54112798+PandaPalumbo@users.noreply.github.com>
2022-11-01 16:13:59 -0400

    added IG feed to home page

--- cold-shoulder-gourmet/src/components/Home/index.js
+++ cold-shoulder-gourmet/src/components/Home/index.js
@@ -5,6 +5,7 @@ import {Col, Container, Row, Spinner} from "react-bootstrap";
 import Info from "../Info";
 import BckGrnd from '../SiteHeaderImageBlock/main_backgorund.png'
 import SiteHeaderImageBlock from "../SiteHeaderImageBlock";
+import InstaGrid from "../InstaGrid";
 
 function Home() {
     const [soldOut, setSoldOut] = useState(false);
@@ -63,7 +64,7 @@ function Home() {
 
             <Row className={"h-50 py-1 px-5"}>
                 <Col>
-                    <Container >
+                    <Container>
                         <Row className={"h-50 p-5 text-center"} style={{fontWeight: 500, letterSpacing: "0.25em"}}>
                             <h1 style={{fontSize: 88}}>
                                 ABOUT THESE BADASS SANDWICHES
@@ -71,29 +72,41 @@ function Home() {
                         </Row>
                         <Row style={{fontSize: 30}}>
                             <p>
-                                At Cold Shoulder Gourmet, we think good food and quality ingredients should be available to everyone
+                                At Cold Shoulder Gourmet, we think good food and quality ingredients should be available
+                                to everyone
                                 at prices that don’t cost an arm, leg or spleen.
                             </p>
                             <p>
-                                So with the help of some amazing local farmers, bakers and candle stick makers, we are able to offer
-                                you amazing fresh focaccia sandwiches with rich, decadent spreads and hand sliced cured meats,
+                                So with the help of some amazing local farmers, bakers and candle stick makers, we are
+                                able to offer
+                                you amazing fresh focaccia sandwiches with rich, decadent spreads and hand sliced cured
+                                meats,
                                 incredible locally grown greens, and delicious cheeses.
                             </p>
                             <p>
-                                Don't let our light humor lull you into a false sense of security, we know what we are doing and we
+                                Don't let our light humor lull you into a false sense of security, we know what we are
+                                doing and we
                                 are here to blow your taste buds away!
                             </p>
                             <p>
-                                Opening in Charleston, SC - we’ve seen plenty of changes in the way people approach food in this
+                                Opening in Charleston, SC - we’ve seen plenty of changes in the way people approach food
+                                in this
                                 city, and we couldn’t be more excited.
                             </p>
                             <p>
-                                Come and check us out in person/order online and discover something deliciously badass today.
+                                Come and check us out in person/order online and discover something deliciously badass
+                                today.
                             </p>
                         </Row>
+                        <hr/>
+                        <Row>
+                            <InstaGrid/>
+                        </Row>
+                        <hr/>
                     </Container>
                 </Col>
             </Row>
+
 
             <Info
                 address={address}
@@ -101,7 +114,7 @@ function Home() {
                 addressSize={25}
                 hoursSize={20}
                 phone={phone}
-                middleComponent={() => null }
+                middleComponent={() => null}
             />
         </Container>
     );
--- cold-shoulder-gourmet/src/components/InstaGrid/index.js
+++ cold-shoulder-gourmet/src/components/InstaGrid/index.js
@@ -1,32 +1,12 @@
-//
-// import * as React from 'react';
-// import withInstagramFeed from 'origen-react-instagram-feed';
-// import { compose } from 'recompose';
-// import {Col, Container} from "react-bootstrap";
-//
-//
-// const InstaGrid = ({ classes, media, account, status}) => {
-//     return (
-//         <Container>
-//             {media &&
-//                 status === 'completed' &&
-//                 media.map(({ displayImage, id, postLink, accessibilityCaption }) => (
-//                     <Col xs={12} sm={6} md={4} key={id || displayImage}>
-//                         <a href={postLink || `https://www.instagram.com/${account}`}>
-//                                 <img
-//                                     src={displayImage}
-//                                     alt={accessibilityCaption || 'Instagram picture'}
-//                                     className={classes.image}
-//                                 />
-//                         </a>
-//                     </Col>
-//                 ))}
-//             {status === 'loading' && <p>loading...</p>}
-//             {status === 'failed' && <p>Check instagram here</p>}
-//         </Container>
-//     );
-// };
-//
-// export default compose(
-//     withInstagramFeed,
-// )(InstaGrid);
+import React from 'react'
+import InstagramFeed  from 'react-ig-feed'
+import 'react-ig-feed/dist/index.css'
+
+const token ="IGQVJXdXVDMlRiZAzQ3MjFIS2VUa2I2bGJwZAEhvcVh2NVFmbTBLV05yREU2bkttOGJ0enJzU0huNVZAEY3ZAYTzc0NHBnUE53bHk3SEZATM094aTVyQXVPWUYtTDIxZAk9VQ21ka0JxZA0hFdzYxX2tYeGJIZAwZDZD"
+
+const InstaGrid = () => {
+    return <InstagramFeed token={token}  counter="24"/>
+}
+
+export default InstaGrid
+
