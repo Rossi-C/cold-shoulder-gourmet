312e47167142cc078439296ae09cd6dc0a33acc1
PandaPalumbo <mattdpalumbo@gmail.com>
2022-11-18 11:26:48 -0500

    igKey retreived from backend

--- cold-shoulder-gourmet/packages/client/src/components/Home/index.js
+++ cold-shoulder-gourmet/packages/client/src/components/Home/index.js
@@ -1,11 +1,11 @@
 import SoldOut from "../SoldOut";
-import {Col, Container, Row } from "react-bootstrap";
+import {Col, Container, Row, Spinner} from "react-bootstrap";
 import Info from "../Info";
 import BckGrnd from '../SiteHeaderImageBlock/main_backgorund.png'
 import SiteHeaderImageBlock from "../SiteHeaderImageBlock";
 import InstaGrid from "../InstaGrid";
 
-function Home({soldOut, address, hours, phone, loading}) {
+function Home({soldOut, address, hours, phone, loading, igKey}) {
     return (
         <Container fluid>
             {
@@ -19,7 +19,7 @@ function Home({soldOut, address, hours, phone, loading}) {
 
                     <Row className={"h-50 py-1 px-5"}>
                         <Col>
-                            <Container >
+                            <Container>
                                 <Row className={"h-50 p-5 text-center"} style={{fontWeight: 500, letterSpacing: "0.25em"}}>
                                     <h1 style={{fontSize: 88}}>
                                         ABOUT THESE BADASS SANDWICHES
@@ -27,31 +27,38 @@ function Home({soldOut, address, hours, phone, loading}) {
                                 </Row>
                                 <Row style={{fontSize: 30}}>
                                     <p>
-                                        At Cold Shoulder Gourmet, we think good food and quality ingredients should be available to everyone
+                                        At Cold Shoulder Gourmet, we think good food and quality ingredients should be
+                                        available to everyone
                                         at prices that don’t cost an arm, leg or spleen.
                                     </p>
                                     <p>
-                                        So with the help of some amazing local farmers, bakers and candle stick makers, we are able to offer
-                                        you amazing fresh focaccia sandwiches with rich, decadent spreads and hand sliced cured meats,
+                                        So with the help of some amazing local farmers, bakers and candle stick makers, we
+                                        are able to offer
+                                        you amazing fresh focaccia sandwiches with rich, decadent spreads and hand sliced
+                                        cured meats,
                                         incredible locally grown greens, and delicious cheeses.
                                     </p>
                                     <p>
-                                        Don't let our light humor lull you into a false sense of security, we know what we are doing and we
+                                        Don't let our light humor lull you into a false sense of security, we know what we
+                                        are doing and we
                                         are here to blow your taste buds away!
                                     </p>
                                     <p>
-                                        Opening in Charleston, SC - we’ve seen plenty of changes in the way people approach food in this
+                                        Opening in Charleston, SC - we’ve seen plenty of changes in the way people approach
+                                        food in this
                                         city, and we couldn’t be more excited.
                                     </p>
                                     <p>
-                                        Come and check us out in person/order online and discover something deliciously badass today.
+                                        Come and check us out in person/order online and discover something deliciously
+                                        badass today.
                                     </p>
                                 </Row>
                             </Container>
                         </Col>
-                        < hr />
-                        {/*<InstaGrid />*/}
-                        < hr />
+                        < hr/>
+
+                            <InstaGrid igKey={igKey}/>
+                        < hr/>
                     </Row>
 
                     <Info
@@ -60,7 +67,7 @@ function Home({soldOut, address, hours, phone, loading}) {
                         addressSize={25}
                         hoursSize={20}
                         phone={phone}
-                        middleComponent={() => null }
+                        middleComponent={() => null}
                     />
                 </>
             }
--- cold-shoulder-gourmet/packages/client/src/components/InstaGrid/index.js
+++ cold-shoulder-gourmet/packages/client/src/components/InstaGrid/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import InstagramFeed  from 'react-ig-feed'
 import 'react-ig-feed/dist/index.css'
+import {Spinner} from "react-bootstrap";
 
-const token ="IGQVJXdXVDMlRiZAzQ3MjFIS2VUa2I2bGJwZAEhvcVh2NVFmbTBLV05yREU2bkttOGJ0enJzU0huNVZAEY3ZAYTzc0NHBnUE53bHk3SEZATM094aTVyQXVPWUYtTDIxZAk9VQ21ka0JxZA0hFdzYxX2tYeGJIZAwZDZD"
+const InstaGrid = ({igKey}) => {
+    return(
+            igKey ? <InstagramFeed token={igKey}  counter="24"/> :
+                <Spinner className="m-auto" animation="border" role="status" variant={"light"}>
+                    <span className="visually-hidden">Loading Instagram...</span>
+                </Spinner>
 
-const InstaGrid = () => {
-    return <InstagramFeed token={token}  counter="24"/>
+    )
 }
 
 export default InstaGrid
