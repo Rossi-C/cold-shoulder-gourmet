34ec3aab4b684203f259d49e5c2c0d9cbde58999
PandaPalumbo <mattdpalumbo@gmail.com>
2022-11-28 15:38:42 -0500

    fixing loader appearance

--- cold-shoulder-gourmet/packages/client/src/App.js
+++ cold-shoulder-gourmet/packages/client/src/App.js
@@ -3,11 +3,11 @@ import {
     Routes,
 } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import { Container, Row, Spinner } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import {Container, Row, Spinner} from "react-bootstrap";
+import {useEffect, useState} from "react";
 import WebFont from "webfontloader";
-import { AiFillInstagram } from "react-icons/ai";
-import { getBusinessInfo, getIG } from "./api";
+import {AiFillInstagram} from "react-icons/ai";
+import {getBusinessInfo, getIG} from "./api";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import Menu from "./components/Menu";
@@ -43,7 +43,7 @@ function App() {
     const updateHomeState = async () => {
         setIgKey(await getIG())
         setLoading(true);
-        const { soldOut, address, hours, phone, winterMenu } = await getBusinessInfo() // returns {souldOut: boolean, address:Object, hours:Object}
+        const {soldOut, address, hours, phone, winterMenu} = await getBusinessInfo() // returns {souldOut: boolean, address:Object, hours:Object}
         if (soldOut || address || hours) {
             setSoldOut(soldOut);
             setAddress(address);
@@ -65,56 +65,61 @@ function App() {
     }, []);
 
     return (
-        <Container className="bg-black px-0 py-2 m-0" style={{ minHeight: "100vh", fontFamily: "Oswald" }} fluid>
+        <Container className="bg-black px-0 py-2 m-0" style={{minHeight: "100vh", fontFamily: "Oswald"}} fluid>
             <div className="text-light">
                 <BrowserRouter>
+
+                    <Navigation/>
                     {
-                        loading &&
-                        <Row className={"h-50 p-5"}>
-                            <Spinner className="m-auto" animation="border" role="status" variant={"light"}>
-                                <span className="visually-hidden">Loading...</span>
-                            </Spinner>
-                        </Row>
+                        loading ?
+                            <Row className={"h-50 p-5"}>
+                                <Spinner className="m-auto" animation="border" role="status" variant={"light"}>
+                                    <span className="visually-hidden">Loading...</span>
+                                </Spinner>
+                            </Row>
+                            :
+                            <Routes>
+                                <Route
+                                    path='/'
+                                    element={<Home soldOut={soldOut} address={address} hours={hours} phone={phone}
+                                                   igKey={igKey}/>}
+                                />
+                                <Route
+                                    path='aboutUs'
+                                    element={<AboutUs/>}
+                                />
+                                <Route
+                                    path='menu'
+                                    element={<Menu winterMenu={winterMenu} loading={loading}/>}
+                                />
+                                <Route
+                                    path='admin'
+                                    element={<Admin soldOut={soldOut} address={address} hours={hours} phone={phone}
+                                                    winterMenu={winterMenu} loading={loading}/>}
+                                />
+                                <Route
+                                    path='producers'
+                                    element={<Producers/>}
+                                />
+                                <Route
+                                    path='team'
+                                    element={<MeetTheTeam/>}
+                                />
+                                <Route
+                                    path='services'
+                                    element={<Services/>}
+                                />
+                                <Route
+                                    path='policies'
+                                    element={<Policies/>}
+                                />
+                            </Routes>
                     }
-                    <Navigation />
-                    <Routes>
-                        <Route
-                            path='/'
-                            element={<Home soldOut={soldOut} address={address} hours={hours} phone={phone} igKey={igKey} />}
-                        />
-                        <Route
-                            path='aboutUs'
-                            element={<AboutUs />}
-                        />
-                        <Route
-                            path='menu'
-                            element={<Menu winterMenu={winterMenu} loading={loading} />}
-                        />
-                        <Route
-                            path='admin'
-                            element={<Admin soldOut={soldOut} address={address} hours={hours} phone={phone} winterMenu={winterMenu} loading={loading} />}
-                        />
-                        <Route
-                            path='producers'
-                            element={<Producers />}
-                        />
-                        <Route
-                            path='team'
-                            element={<MeetTheTeam />}
-                        />
-                        <Route
-                            path='services'
-                            element={<Services />}
-                        />
-                        <Route
-                            path='policies'
-                            element={<Policies />}
-                        />
-                    </Routes>
+
                 </BrowserRouter>
             </div>
             <div>
-                <hr className={'text-light'} />
+                <hr className={'text-light'}/>
                 <div className={'m-auto text-center text-light'}>
                     <h1>COLD SHOULDER GOURMET</h1>
                     <p>craigedmunds@coldshouldergourmet.com</p>
@@ -122,7 +127,7 @@ function App() {
                     <p>1684 Old Towne Rd Charleston, SC</p>
                     <p>
                         <a className={"link-light"} href="https://www.instagram.com/coldshouldergourmet/">
-                            <AiFillInstagram style={{ fontSize: 30 }} />
+                            <AiFillInstagram style={{fontSize: 30}}/>
                         </a>
                     </p>
                 </div>
